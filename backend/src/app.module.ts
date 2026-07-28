c9838bcf0bfe8514caae385be485a626eb499e75
Raj Narayan Singh <avhiadi75@gmail.com>
2026-07-29 00:12:21 +0530

    chore: remove temporary /seed endpoint

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -7,7 +7,6 @@ import { UsersModule } from './users/users.module';
 import { CustomersModule } from './customers/customers.module';
 import { ProductsModule } from './products/products.module';
 import { ChallansModule } from './challans/challans.module';
-import { SeedModule } from './seed/seed.module';
 
 @Module({
   imports: [
@@ -22,7 +21,6 @@ import { SeedModule } from './seed/seed.module';
     CustomersModule,
     ProductsModule,
     ChallansModule,
-    SeedModule,
   ],
 })
 export class AppModule {}
